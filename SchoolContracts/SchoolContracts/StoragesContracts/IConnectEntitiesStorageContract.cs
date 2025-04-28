b7db13e4efcdefbbdd3688e068db6a0f85182200
Adelina888 <adelinachalikova@gmail.com>
2025-04-28 22:14:12 +0400

    .

--- SchoolContracts/SchoolContracts/StoragesContracts/IConnectEntitiesStorageContract.cs
+++ SchoolContracts/SchoolContracts/StoragesContracts/IConnectEntitiesStorageContract.cs
@@ -9,4 +9,9 @@ public interface IConnectEntitiesStorageContract
     public void CreateConnectCircleAndMaterial (string circleId, string materialId, int count = 1);
     public void DeleteConnectCircleAndMaterial (string circleId, string materialId, int count = 1);
 
+    public void CreateConnectLessonAndInterest(string lessonId, string interestId);
+    public void DeleteConnectLessonAndInterest(string lessonId, string interestId);
+    public void CreateConnectInterestAndMaterial(string interestId, string materialId);
+    public void DeleteConnectInterestAndMaterial(string interestId, string materialId);
+
 }
