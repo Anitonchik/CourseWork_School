93c91763e60ab9899f564b3b9b7cf6ff7181602d
Anitonchik <anitonchikantonova@yandex.ru>
2025-04-28 19:06:24 +0400

    надо было закинуть эх

---
+++ SchoolContracts/SchoolContracts/ModelsForReports/CirclesWithInterestsWithMedals.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolContracts.ModelsForReports;
+
+public class CirclesWithInterestsWithMedals
+{
+    public string CircleName { get; set; }
+    public string CircleDescription { get; set; }
+    public string InterestName {  get; set; }
+    public string MedalName {  get; set; }
+}
--- SchoolContracts/SchoolDatabase/Implementations/CircleStorageContract.cs
+++ SchoolContracts/SchoolDatabase/Implementations/CircleStorageContract.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using NUnit.Framework.Internal;
 using SchoolContracts.DataModels;
 using SchoolContracts.Exceptions;
+using SchoolContracts.ModelsForReports;
 using SchoolContracts.StoragesContracts;
 using SchoolDatabase.Models;
+using SchoolDatabase.Models.ModelsForReports;
 
 namespace SchoolDatabase.Implementations;
 
@@ -35,33 +37,21 @@ public class CircleStorageContract : ICircleStorageContract
         }
     }
 
-    public List<CircleDataModel> GetCirclesWithInterestsWithMedals(DateTime fromDate, DateTime toDate)
+    public List<CirclesWithInterestsWithMedals> GetCirclesWithInterestsWithMedals(DateTime fromDate, DateTime toDate)
     {
-        /*SELECT c."Id", c."StorekeeperId", c."CircleName", c."Description",
-md."MedalName", i."InterestName", l."LessonDate"
-FROM "Circles" c
-JOIN "CircleMaterials" cm ON c."Id" = cm."CircleId"
-JOIN "Materials" mt ON cm."MaterialId" = mt."Id"
-JOIN "Medals" md ON md."MaterialId" = cm."MaterialId"
-JOIN "InterestMaterials" im ON im."MaterialId" = mt."Id"
-JOIN "Interests" i ON i."Id" = im."InterestId"
-JOIN "LessonInterests" li ON li."InterestId" = i."Id"
+        var sql = $"SELECT c.\"CircleName\" as \"CircleName\", c.\"Description\" as \"CircleDescription\", " +
+            $"i.\"InterestName\" as \"InterestName\", md.\"MedalName\" as \"MedalName\" " +
+            $"FROM \"Circles\" c" +
+            $"JOIN \"CircleMaterials\" cm ON c.\"Id\" = cm.\"CircleId\" " +
+            $"JOIN \"Materials\" mt ON cm.\"MaterialId\" = mt.\"Id\" " +
+            $"JOIN \"Medals\" md ON md.\"MaterialId\" = cm.\"MaterialId\" " +
+            $"JOIN \"InterestMaterials\" im ON im.\"MaterialId\" = mt.\"Id\" " +
+            $"JOIN \"Interests\" i ON i.\"Id\" = im.\"InterestId\" " +
+            $"JOIN \"LessonInterests\" li ON li.\"InterestId\" = i.\"Id\" " +
+            $"JOIN \"Lessons\" l ON l.\"Id\" = li.\"LessonId\" " +
+            $"WHERE(l.\"LessonDate\" between {fromDate} and {toDate});";
 
-JOIN "Lessons" l ON l."Id" = li."LessonId"
-WHERE(l."LessonDate" = 'date');*/
-
-        var circles = (from c in _dbContext.Circles
-                      join cm in _dbContext.CircleMaterials on c.Id equals cm.MaterialId
-                      join mt in _dbContext.Materials on cm.MaterialId equals mt.Id
-                      join md in _dbContext.Medals on cm.MaterialId equals md.MaterialId
-                      join im in _dbContext.InterestMaterials on mt.Id equals im.MaterialId
-                      join i in _dbContext.Interests on im.InterestId equals i.Id
-                      join li in _dbContext.LessonInterests on i.Id equals li.InterestId
-                      join l in _dbContext.Lessons on li.LessonId equals l.Id
-                      where l.LessonDate >= fromDate && l.LessonDate <= toDate
-                      select c).ToList();
-
-        return [.. circles.Select(x => _mapper.Map<CircleDataModel>(x))];
+        return _dbContext.Set<CirclesWithInterestsWithMedals>().FromSqlRaw(sql).ToList();
 
     }
 
