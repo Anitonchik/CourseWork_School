965e9389e0071db39b3a0907dfd23776e9fd0c42
Adelina888 <adelinachalikova@gmail.com>
2025-05-12 16:16:34 +0400

    .

--- SchoolContracts/SchoolContracts/DataModels/LessonInterestDataModel.cs
+++ SchoolContracts/SchoolContracts/DataModels/LessonInterestDataModel.cs
@@ -12,5 +12,16 @@ public class LessonInterestDataModel(string lessonId, string interesId,string ca
 {
     public string LessonId { get; private set; } = lessonId;
     public string InterestId { get; private set; } = interesId;
-    
+    public string Category { get; private set; } = category;
+    public void Validate()
+    {
+        if (LessonId.IsEmpty())
+            throw new ValidationException("Field LessonId is empty");
+        if (!LessonId.IsGuid())
+            throw new ValidationException("The value in the field OrderId is not a unique identifier");
+        if (InterestId.IsEmpty())
+            throw new ValidationException("Field InterestId is empty");
+        if (!InterestId.IsGuid())
+            throw new ValidationException("The value in the field Id is not a unique identifier");
+    }
 }
