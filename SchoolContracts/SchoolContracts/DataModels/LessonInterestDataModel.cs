b9b98e3e4d75d4f1995a0a41544c5ea15a1403a5
Adelina888 <adelinachalikova@gmail.com>
2025-05-12 16:11:27 +0400

    многие ко многим+убрала лишнее

--- SchoolContracts/SchoolContracts/DataModels/LessonInterestDataModel.cs
+++ SchoolContracts/SchoolContracts/DataModels/LessonInterestDataModel.cs
@@ -8,20 +8,9 @@ using System.Threading.Tasks;
 
 namespace SchoolContracts.DataModels;
 
-public class LessonInterestDataModel(string lessonId, string interesId)
+public class LessonInterestDataModel(string lessonId, string interesId,string category)
 {
     public string LessonId { get; private set; } = lessonId;
     public string InterestId { get; private set; } = interesId;
-
-    public void Validate()
-    {
-        if (LessonId.IsEmpty())
-            throw new ValidationException("Field LessonId is empty");
-        if (!LessonId.IsGuid())
-            throw new ValidationException("The value in the field OrderId is not a unique identifier");
-        if (InterestId.IsEmpty())
-            throw new ValidationException("Field InterestId is empty");
-        if (!InterestId.IsGuid())
-            throw new ValidationException("The value in the field Id is not a unique identifier");
-    }
+    
 }
